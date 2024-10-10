68529c7c41d8db9beb3070b3e26096f9a3c6f81b
TZane99 <tomzaneco@gmail.com>
2024-10-10 19:09:02 -0400

    Starter asteroids game

--- main.py
+++ main.py
@@ -1,22 +1,69 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         return
-    running = True
-    while running:
+    font = pygame.font.Font(None, 36)
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    score = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    pygame.sprite.Sprite.kill(shot)
+                    score += 10
+
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+            
+            
+        text_surface = font.render(f"Score: {score}", True, (255, 255, 255))
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (SCREEN_WIDTH / 2, 50)  # Position the score on the screen
+        screen.blit(text_surface, text_rect)
+        
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
